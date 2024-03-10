481bda5b5d44c89b1b96d67d135190df213f9877
Crash285 <jackadam852@gmail.com>
2024-03-10 02:20:45 +0100

    about page

--- lib/view/pages/home_page/drawer/drawer.dart
+++ lib/view/pages/home_page/drawer/drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ytp_new/service/app_navigator.dart';
+import 'package:ytp_new/view/pages/about/about_page.dart';
 import 'package:ytp_new/view/pages/home_page/drawer/settings/reorder_toggle.dart';
 import 'package:ytp_new/view/pages/home_page/drawer/settings/scheme_mode.dart';
 import 'package:ytp_new/view/pages/home_page/drawer/settings/split_mode.dart';
@@ -32,6 +34,27 @@ class HomePageDrawer extends StatelessWidget {
                 ],
               ),
             ),
+            const Divider(indent: 8.0, endIndent: 8.0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 8.0),
+              child: InkWell(
+                onTap: () => AppNavigator.tryPushLeft(const AboutPage()),
+                borderRadius: BorderRadius.circular(16.0),
+                overlayColor: MaterialStatePropertyAll(
+                    Theme.of(context).colorScheme.primary.withOpacity(.3)),
+                child: const Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("About"),
+                      Icon(Icons.info_outline),
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
