6fed5465833d09a09bb2ec75421d3a2800aeb8c2
adam.horvath <adam.horvath@aimotive.com>
2024-03-12 20:41:14 +0100

    planned notfloating

--- lib/view/pages/playlist_page/tabs/videos/planned_sheet/planned_sheet.dart
+++ lib/view/pages/playlist_page/tabs/videos/planned_sheet/planned_sheet.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:ytp_new/model/playlist/playlist.dart';
@@ -23,78 +23,86 @@ class PlannedSheet extends StatefulWidget {
 
 class _PlannedSheetState extends State<PlannedSheet> {
   late final DraggableScrollableController _controller;
-  double bottomMargin = 8.0;
-
+  bool showDim = false;
   Playlist get playlist => PlaylistStorageProvider().fromId(widget.playlistId)!;
 
   @override
   void initState() {
     super.initState();
     _controller = DraggableScrollableController()
-      ..addListener(
-        () => setState(
-          () => bottomMargin = max(0, 8 - (_controller.size * 9)),
-        ),
-      );
+      ..addListener(() {
+        setState(() {
+          showDim = _controller.pixels >= 100;
+        });
+      });
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        final double minSize = 64 / constraints.maxHeight;
+        final double minSize = 60 / constraints.maxHeight;
+
         final double maxSize = 1 - minSize;
 
-        return DraggableScrollableSheet(
-          controller: _controller,
-          minChildSize: minSize,
-          initialChildSize: minSize,
-          maxChildSize: maxSize,
-          snap: true,
-          builder: (context, scrollController) => Center(
-            child: Card(
-              elevation: 16,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                topLeft: const Radius.circular(8.0),
-                topRight: const Radius.circular(8.0),
-                bottomLeft: Radius.circular(bottomMargin),
-                bottomRight: Radius.circular(bottomMargin),
-              )),
-              margin: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, bottomMargin),
-              child: ScrollConfiguration(
-                behavior: const MaterialScrollBehavior().copyWith(
-                  scrollbars: false,
-                ),
-                child: FadingListView(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  itemBuilder: (context, index) {
-                    //? Title & handle
-                    if (index == 0) {
-                      return PlannedSheetTitle(
-                        playlistId: playlist.id,
-                        onTap: () {
-                          _controller.animateTo(
-                            _controller.size <= .5 ? maxSize : minSize,
-                            duration: Durations.short4,
-                            curve: Curves.decelerate,
+        return Stack(
+          children: [
+            if (showDim)
+              Container(
+                color: Colors.transparent,
+              ),
+            DraggableScrollableSheet(
+              controller: _controller,
+              minChildSize: minSize,
+              initialChildSize: minSize,
+              maxChildSize: maxSize,
+              snap: true,
+              builder: (context, scrollController) => Center(
+                child: Card(
+                  elevation: 16,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(8.0),
+                    topRight: Radius.circular(8.0),
+                    bottomLeft: Radius.zero,
+                    bottomRight: Radius.zero,
+                  )),
+                  margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                  child: ScrollConfiguration(
+                    behavior: const MaterialScrollBehavior().copyWith(
+                      scrollbars: false,
+                    ),
+                    child: FadingListView(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      itemBuilder: (context, index) {
+                        //? Title & handle
+                        if (index == 0) {
+                          return PlannedSheetTitle(
+                            playlistId: playlist.id,
+                            onTap: () {
+                              _controller.animateTo(
+                                _controller.size <= .5 ? maxSize : minSize,
+                                duration: Durations.short4,
+                                curve: Curves.decelerate,
+                              );
+                            },
                           );
-                        },
-                      );
-                    }
+                        }
 
-                    //? items
-                    return PlannedItem(
-                      playlistId: widget.playlistId,
-                      text: widget.planned.elementAt(index - 1),
-                    );
-                  },
-                  itemCount: widget.planned.length + 1,
-                  controller: scrollController,
+                        //? items
+                        return PlannedItem(
+                          playlistId: widget.playlistId,
+                          text: widget.planned.elementAt(index - 1),
+                        );
+                      },
+                      itemCount: widget.planned.length + 1,
+                      controller: scrollController,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         );
       },
     );
