4b240c7c316fdcfe9aed6d838f514cd0dddedb06
Crash285 <jackadam852@gmail.com>
2024-03-24 02:28:47 +0100

    hostory grouping

--- lib/view/pages/playlist_page/tabs/history/tab_history.dart
+++ lib/view/pages/playlist_page/tabs/history/tab_history.dart
@@ -26,12 +26,30 @@ class _PlaylistPageTabHistoryState extends State<PlaylistPageTabHistory>
             gradientHeight: 70,
             bottom: false,
             padding: const EdgeInsets.only(bottom: 80, top: 20),
-            itemBuilder: (context, index) => HistoryItem(
-              playlistId: widget.playlistId,
-              history: history[index],
-              isFirst: index == 0,
-              isLast: index == history.length - 1,
-            ),
+            itemBuilder: (context, index) {
+              bool first = true;
+              bool last = true;
+              try {
+                last = history[index].created != history[index + 1].created;
+              } catch (_) {}
+
+              try {
+                first = history[index - 1].created != history[index].created;
+              } catch (_) {}
+
+              return Padding(
+                padding: EdgeInsets.only(
+                  top: first ? 4.0 : 0,
+                  bottom: last ? 4.0 : 0,
+                ),
+                child: HistoryItem(
+                  playlistId: widget.playlistId,
+                  history: history[index],
+                  isFirst: first,
+                  isLast: last,
+                ),
+              );
+            },
             itemCount: history.length,
           );
   }
