1978a8d529ab2aad265919b62fc4c14fd422390b
Crash285 <jackadam852@gmail.com>
2024-04-01 10:28:53 +0200

    set planned sheet max width

--- lib/view/pages/playlist_page/tabs/videos/planned_sheet/planned_sheet.dart
+++ lib/view/pages/playlist_page/tabs/videos/planned_sheet/planned_sheet.dart
@@ -48,46 +48,49 @@ class _PlannedSheetState extends State<PlannedSheet> {
           maxChildSize: maxSize,
           snap: true,
           builder: (context, scrollController) => Center(
-            child: Card(
-              elevation: 16,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(16.0),
-                  topRight: Radius.circular(16.0),
-                  bottomLeft: Radius.zero,
-                  bottomRight: Radius.zero,
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 500),
+              child: Card(
+                elevation: 16,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                    bottomLeft: Radius.zero,
+                    bottomRight: Radius.zero,
+                  ),
                 ),
-              ),
-              margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-              child: ScrollConfiguration(
-                behavior: const MaterialScrollBehavior().copyWith(
-                  scrollbars: false,
-                ),
-                child: ListView.builder(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  itemBuilder: (context, index) {
-                    //? Title & handle
-                    if (index == 0) {
-                      return PlannedSheetTitle(
-                        playlistId: playlist.id,
-                        onTap: () {
-                          _controller.animateTo(
-                            _controller.size <= .5 ? maxSize : minSize,
-                            duration: AppConfig.defaultAnimationDuration,
-                            curve: Curves.fastOutSlowIn,
-                          );
-                        },
-                      );
-                    }
+                margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                child: ScrollConfiguration(
+                  behavior: const MaterialScrollBehavior().copyWith(
+                    scrollbars: false,
+                  ),
+                  child: ListView.builder(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    itemBuilder: (context, index) {
+                      //? Title & handle
+                      if (index == 0) {
+                        return PlannedSheetTitle(
+                          playlistId: playlist.id,
+                          onTap: () {
+                            _controller.animateTo(
+                              _controller.size <= .5 ? maxSize : minSize,
+                              duration: AppConfig.defaultAnimationDuration,
+                              curve: Curves.fastOutSlowIn,
+                            );
+                          },
+                        );
+                      }
 
-                    //? items
-                    return PlannedItem(
-                      playlistId: widget.playlistId,
-                      text: widget.planned.elementAt(index - 1),
-                    );
-                  },
-                  itemCount: widget.planned.length + 1,
-                  controller: scrollController,
+                      //? items
+                      return PlannedItem(
+                        playlistId: widget.playlistId,
+                        text: widget.planned.elementAt(index - 1),
+                      );
+                    },
+                    itemCount: widget.planned.length + 1,
+                    controller: scrollController,
+                  ),
                 ),
               ),
             ),
