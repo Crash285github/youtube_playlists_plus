adb12f946b9a7cd2fb09464bd61d9ff12eeadfa2
Crash285 <jackadam852@gmail.com>
2024-04-03 21:39:49 +0200

    v1.0.2

--- lib/model/playlist_storage.dart
+++ lib/model/playlist_storage.dart
@@ -4,17 +4,17 @@ part of persistence;
 class PlaylistStorage {
   static final List<Playlist> _playlists = [];
 
-  /// Returns all [Playlists] as an unmodifiable List
+  /// Returns all [Playlist]s as an unmodifiable List
   static List<Playlist> get playlists => List.unmodifiable(_playlists);
 
   /// Adds a [Playlist] to the Storage
-  static void add(Playlist pl) => _playlists.add(pl);
+  static void add(final Playlist pl) => _playlists.add(pl);
 
   /// Removes a [Playlist] from the Storage
-  static bool remove(Playlist pl) => _playlists.remove(pl);
+  static bool remove(final Playlist pl) => _playlists.remove(pl);
 
-  /// Replaces all [Playlists] from the Storage
-  static void replace(List<Playlist> playlists) => _playlists
+  /// Replaces all [Playlist]s from the Storage
+  static void replace(final List<Playlist> playlists) => _playlists
     ..clear()
     ..addAll(playlists);
 }
